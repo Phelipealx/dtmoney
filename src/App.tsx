78a9576de8c74e5144250a9b6e0cd371cf8717ac
Phelipe Magalhães <phelipeamagalhaes@gmail.com>
2022-02-10 09:09:26 -0300

    implementa hook de context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
@@ -18,7 +19,7 @@ export function App() {
 
   const teste: string = "teste";
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -27,6 +28,6 @@ export function App() {
         teste={teste}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
